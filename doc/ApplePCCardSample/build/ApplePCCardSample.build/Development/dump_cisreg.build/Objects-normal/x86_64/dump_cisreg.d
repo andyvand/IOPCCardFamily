0ce08627b5a511017511da1bbee5eb230393bf75
Joris Schepers <joris@imac.home>
2015-01-23 00:42:38 +0100

    Initial commit

---
+++ doc/ApplePCCardSample/build/ApplePCCardSample.build/Development/dump_cisreg.build/Objects-normal/x86_64/dump_cisreg.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Users/joris/Downloads/IOPCCardFamily-48/doc/ApplePCCardSample/dump_cisreg.c \
+  /Users/joris/Downloads/IOPCCardFamily-48/doc/ApplePCCardSample/../../pccard/cs_types.h \
+  /Users/joris/Downloads/IOPCCardFamily-48/doc/ApplePCCardSample/../../pccard/cs.h \
+  /Users/joris/Downloads/IOPCCardFamily-48/doc/ApplePCCardSample/../../pccard/cistpl.h \
+  /Users/joris/Downloads/IOPCCardFamily-48/doc/ApplePCCardSample/../../pccard/cisreg.h
